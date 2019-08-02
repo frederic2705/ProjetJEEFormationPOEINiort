69865d2cd03dcf0f218b9e32db408e0dc45fb520
ftriguer2019 <ftriguer2019@138-UC51-04.ad.campus-eni.fr>
2019-08-02 12:14:35 +0200

    commit

--- ProjetJEE/src/projetjee/servlet/ServletNosPlats.java
+++ ProjetJEE/src/projetjee/servlet/ServletNosPlats.java
@@ -6,6 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import projetjee.bll.CommentaireManager;
+import projetjee.bo.Commentaire;
+import projetjee.bo.User;
 
 /**
  * Servlet implementation class ServletNosPlats
@@ -35,7 +40,15 @@ public class ServletNosPlats extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String note = request.getParameter("noteUser");
+		String comment = request.getParameter("comUser");
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("currentSessionUser");
+		int id;
+		id = user.getId();
+		CommentaireManager rm = new CommentaireManager();
+		Commentaire commentaire;
+		
 	}
 
 }
